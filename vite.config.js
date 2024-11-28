ac1606936ae14848bca92162ecdc0327ec37bb19
LeandroMQ2018 <deinermamani34@gmail.com>
2024-11-27 23:45:03 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,15 +3,11 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
   server: {
-    port: process.env.PORT,5173 // Usa el puerto asignado por Render, o 5173 si no está configurado.
+    port: process.env.PORT || 5173, // Usa el puerto proporcionado por Render
+    host: true, // Asegúrate de que el servidor esté accesible desde cualquier IP
   },
   build: {
-    outDir: 'dist', // Define el directorio de salida para producción
-    sourcemap: true, // Incluye los mapas de origen para depuración
-  },
-  define: {
-    'process.env': process.env, // Asegura que las variables de entorno estén accesibles
+    outDir: 'dist', // Asegúrate de que el directorio de salida sea 'dist'
   },
 });
