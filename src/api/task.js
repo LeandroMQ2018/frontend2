1e30e7760bb903624af4064a92c9c7ee5f05a5ac
LeandroMQ2018 <deinermamani34@gmail.com>
2024-11-27 23:24:18 -0400

    Update task.js

--- src/api/task.js
+++ src/api/task.js
@@ -1,7 +1,7 @@
 // src/api/task.js
 
 // URL base para la API, donde se realizarán las solicitudes
-const API_URL = "https://backend2-h2re.onrender.com";
+const API_URL = import.meta.env.VITE_API_URL || "https://backend2-h2re.onrender.com/api";
 
 // Función para crear una nueva tarea en un proyecto específico
 export const createTask = async (projectId, taskData, token) => {
