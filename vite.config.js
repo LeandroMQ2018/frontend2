671289c87ad1304eb5671a0fccdf3b7e2041be52
LeandroMQ2018 <deinermamani34@gmail.com>
2024-11-27 23:14:05 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    // Define la URL del backend a través de una variable de entorno
-    'process.env.BACKEND_URL': JSON.stringify(process.env.BACKEND_URL || 'https://backend2-h2re.onrender.com'),
+  server: {
+    port: process.env.PORT || 5173, // Usa el puerto asignado por Render, o 5173 si no está configurado.
   },
-});
+})
